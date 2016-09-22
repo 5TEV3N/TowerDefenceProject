2bfc25ab7b700e104c62fdf7f84d2e73a0a540b9
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-22 12:59:49 -0400

    First Commit

---
+++ Assets/Scripts/_TODO.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _TODO : MonoBehaviour
+{
+    /*
+     * Due october 6 
+     * 
+     * Must haves:
+     * Enemies passing through "warzone"    10
+     * Enemy detection  10
+     * Tower shoots at detected enemies 20
+     * Enemies can be killed    10
+     * Game logic = Win/Lose    10
+     * 
+     * Nice to haves:
+     * Follow spline(s) or use navmesh  5
+     * Damage/Health system 5
+     * Smart enemy target selection 5
+     * Wave Engine  5
+     * UI   2.5
+     * Towers can be killed 2.5
+     * Enemies can shoot back / hit back    5
+     * Game Progression (different enemy / tower)   5
+     * Originality  5
+     */
+}
