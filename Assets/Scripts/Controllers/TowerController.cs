b422c05108743700f1cbb37521d5387c2ad73b11
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-22 21:27:17 -0400

    Tower now shoots properly, needs to constantly shoot now

--- Assets/Scripts/Controllers/TowerController.cs
+++ Assets/Scripts/Controllers/TowerController.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class TowerController : MonoBehaviour
 {
-    //BulletController bulletController;
     public GameObject bullet;
     public Transform bulletExitPoint;
+    public float bulletShootingForce;
 
-    public void ShootOnSight()
+    public void ShootOnSight(GameObject drones)
     {
         GameObject bulletShoot = GameObject.Instantiate(bullet);    //Spawn bullet while enemies stay on your sensor
         bulletShoot.transform.position = bulletExitPoint.transform.position; //Spawn this bullet on the same transform as the gameobject that this script is attached
 
-        //Vector3 shootingDirection = bulletShoot.transform.position - bulletExitPoint.transform.position;
-        //shootingDirection.Normalize();
+        Vector3 shootingDirection = drones.transform.position - bulletExitPoint.transform.position;
+        shootingDirection.Normalize();
+
+        bulletShoot.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletShootingForce, ForceMode.Impulse);
 
     }
 
--- Assets/Scripts/Managers/SensorManager.cs
+++ Assets/Scripts/Managers/SensorManager.cs
@@ -4,16 +4,17 @@ using System.Collections;
 public class SensorManager : MonoBehaviour
 {
     TowerController towerController; // Reff to the TowerController Script
+    public GameObject drones; //    This is the enemy that the sensor senses
 
     void Awake()
     {
         towerController = GameObject.FindGameObjectWithTag("T_Towers").GetComponent<TowerController>();
     }
 
-    public void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        towerController.ShootOnSight();
-        print("asdf");
+        towerController.ShootOnSight(drones);
+        print("Drone Entered Tower's Field");
     }
 }
 // When an object is in range, detect it and relay info to shoot > ontrigger enter/stay/exit
