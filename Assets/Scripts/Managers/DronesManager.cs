e9193dfdacc9c9385f214d569ac33e6653c8a6fd
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-26 17:04:38 -0400

    Baked lighting, R key resets level, deleted drones

--- Assets/Scripts/Managers/DronesManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DronesManager : MonoBehaviour
-{
-    //Takes care of drone spawning and such
-
-}
--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class InputManager : MonoBehaviour
 {
-
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);  //Reset
+        }
+	}
 }
