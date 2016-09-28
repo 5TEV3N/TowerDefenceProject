97edfea71acfa89d00df343d130d2f138f82c9fd
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2016-09-28 12:48:01 -0400

    Added Base Manager and TowerSpawnerManager

--- Assets/Scripts/Managers/BaseManager.cs
+++ Assets/Scripts/Managers/BaseManager.cs
@@ -4,4 +4,12 @@ using System.Collections;
 public class BaseManager : MonoBehaviour
 {
     // click on base (cookie clicker style) to get more points. points are used to buy more powerups(?)
+    public int playerResource;          // Keeps track of how much resources
+
+
+    void OnMouseDown()
+    {
+        print("+1 Resouce");
+        playerResource++;
+    }
 }
---
+++ Assets/Scripts/Managers/TowerSpawnerManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerSpawnerManager : MonoBehaviour
+{
+    //When the spawners are clicked on, spawn a tower. BUT player must have the sufficient resources
+
+}
